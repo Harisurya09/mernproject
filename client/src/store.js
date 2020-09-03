261e10967a7c587478498bb44680935f88bea0e4
harisurya <harisurya09@gmail.com>
2020-09-03 12:16:29 +0530

    store file changes in mern

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './redux/rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(logger, thunk))
-    // applyMiddleware(thunk)
+    // composeWithDevTools(applyMiddleware(logger, thunk))
+    applyMiddleware(thunk)
   )
 
 export default store;
